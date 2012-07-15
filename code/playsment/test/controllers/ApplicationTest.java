b813ef484db6db97ae71d0f03294cc55cd0db7bc
Arjun Khandelwal <aakhandelwal@gmail.com>
2012-07-16 00:14:30 +0530

    Adding fist unit test

---
+++ code/playsment/test/controllers/ApplicationTest.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import org.junit.Test;
+import play.mvc.Result;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.*;
+import static play.test.Helpers.callAction;
+
+
+public class ApplicationTest {
+
+    @Test
+    public void callIndex() {
+        Result result = callAction(
+                controllers.routes.ref.Application.index()
+        );
+        assertThat(status(result)).isEqualTo(OK);
+        assertThat(contentType(result)).isEqualTo("text/plain");
+        assertThat(charset(result)).isEqualTo("utf-8");
+        assertThat(contentAsString(result)).contains("This is Playsment home page");
+    }
+}
